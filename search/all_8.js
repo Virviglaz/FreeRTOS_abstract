2dc44cd3a98aa10b099c350ff09d9e7e80b9ee35
Virviglaz <Virviglaz@users.noreply.github.com>
2024-01-05 13:15:38 +0000

    deploy: 39e524dc92c8d67a1c1eda7ad5e099147779c88e

--- search/all_8.js
+++ search/all_8.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['queue_0',['Queue',['../class_free_r_t_o_s_1_1_queue.html',1,'FreeRTOS::Queue&lt; T, size, support_emplace &gt;'],['../class_free_r_t_o_s_1_1_queue.html#ac19f2c7e1e8af74783384f6db93cc812',1,'FreeRTOS::Queue::Queue()']]]
+  ['queue_0',['Queue',['../class_free_r_t_o_s_1_1_queue.html',1,'FreeRTOS::Queue&lt; T, size, support_emplace &gt;'],['../class_free_r_t_o_s_1_1_queue.html#ac19f2c7e1e8af74783384f6db93cc812',1,'FreeRTOS::Queue::Queue()'],['../class_free_r_t_o_s_1_1_queue.html#adff4d860cb39c2ff5ff40277b14e283a',1,'FreeRTOS::Queue::Queue(const Queue &amp;)=delete']]]
 ];
--- search/all_b.js
+++ search/all_b.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['task_0',['Task',['../class_free_r_t_o_s_1_1_task.html',1,'FreeRTOS::Task&lt; stack_size &gt;'],['../class_free_r_t_o_s_1_1_task.html#a9522e36f2028a89978c5f37c78616a74',1,'FreeRTOS::Task::Task()']]],
+  ['task_0',['Task',['../class_free_r_t_o_s_1_1_task.html',1,'FreeRTOS::Task&lt; stack_size &gt;'],['../class_free_r_t_o_s_1_1_task.html#a9522e36f2028a89978c5f37c78616a74',1,'FreeRTOS::Task::Task(void(*handler)(void *), void *params=nullptr, const char *name=nullptr, int priority=tskIDLE_PRIORITY+1)'],['../class_free_r_t_o_s_1_1_task.html#ac5c09246df5766e249ef8fef921b5c6f',1,'FreeRTOS::Task::Task(const Task &amp;)=delete']]],
   ['tryemplaceback_1',['TryEmplaceBack',['../class_free_r_t_o_s_1_1_queue.html#a9bfabf4f64d92bde6700451e17d2909f',1,'FreeRTOS::Queue']]],
   ['tryemplacefront_2',['TryEmplaceFront',['../class_free_r_t_o_s_1_1_queue.html#a703ab0a1c19ff40ca535081729c14460',1,'FreeRTOS::Queue']]],
   ['trypop_3',['TryPop',['../class_free_r_t_o_s_1_1_queue.html#ae3c2c7c25301e6cf29e40f8edf9f3a37',1,'FreeRTOS::Queue']]],
